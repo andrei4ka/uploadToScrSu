1dfbf3c153d71c00b1e6aa51501254653a43acea
Andrey Kirilochkin <akirilochkin@mirantis.com>
2016-05-11 18:56:40 +0300

    refactoring to understand russian filenames and paths with backslashes

--- upload.py
+++ upload.py
@@ -3,18 +3,19 @@
 import sys
 import requests
 import os
+import urllib
 
 apiUrl = 'http://scr.su/upload.php'
 quiet = False
 
 def main():
     for arg in sys.argv[1:]:
-        upload(arg)
+        upload(os.path.normpath(arg))
 
 def upload(file, url=apiUrl):
     try:
         with open(file, 'rb') as f:
-            files = {'image': (file, f)}
+            files = {'image': ("file", f)}
             r = requests.get(url)
             if r.status_code == 200:
                 s = requests.Session()
@@ -24,14 +25,12 @@ def upload(file, url=apiUrl):
                 else:
                     print(postfile.json()["status"], ': ', postfile.json()["data"])
                 s.close()
-
     except FileNotFoundError:
         print('File not found at the path:', file)
+
 def info(info):
-    command = 'echo {} | /usr/bin/pbcopy'
-    command1 = '/usr/local/bin/terminal-notifier -title "Uploaded to scr.su" -message {} -open {}'
-    os.system(command.format(info))
-    os.system(command1.format(info, info))
+    os.system('echo %s | /usr/bin/pbcopy' % info)
+    os.system('/usr/local/bin/terminal-notifier -title "Uploaded to scr.su" -message %s -open %s' % (info, info))
     os.system('afplay /System/Library/Sounds/Hero.aiff')
     if not quiet:
         print(info)
